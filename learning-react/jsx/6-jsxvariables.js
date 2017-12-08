97b2a9b9d66f486dcec78728fd8e88046469a210
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-07 21:54:25 -0800

    varibale in jsx

--- learning-react/jsx/6-jsxvariables.js
+++ learning-react/jsx/6-jsxvariables.js
@@ -32,3 +32,10 @@ ReactDOM.render(
     myList,
     document.getElementById('app')
 );
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const theBestString = 'tralalalala i am da best';
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
--- learning-react/jsx/advancejsx/2-curlybracesjsx.js
+++ learning-react/jsx/advancejsx/2-curlybracesjsx.js
@@ -19,3 +19,12 @@ ReactDOM.render(
 );
 
 // Output 5
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const math = <h1>2 + 3 = {2 + 3}</h1>;
+
+ReactDOM.render(math, document.getElementById('app')
+);
