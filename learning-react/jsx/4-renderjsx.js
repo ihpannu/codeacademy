201930b8a3ce4a2ae992c7f91de406e2642ba184
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-07 20:38:47 -0800

    react js

---
+++ learning-react/jsx/4-renderjsx.js
@@ -0,0 +1,11 @@
+// Rendering JSX
+
+// To render a JSX expression means to make it appear onscreen.
+
+// JavaScript is case -sensitive, so make sure to capitalize ReactDOM correctly!
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('app'));
