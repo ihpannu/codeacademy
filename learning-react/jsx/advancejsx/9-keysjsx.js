60cabf2ffdf2e4681f1cf18c72cfb63c49b24d72
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-08 00:30:43 -0800

    changes

--- learning-react/jsx/advancejsx/9-keysjsx.js
+++ learning-react/jsx/advancejsx/9-keysjsx.js
@@ -14,7 +14,7 @@
 // If you don't use keys when you're supposed to, React might accidentally scramble your list-items 
 // into the wrong order.
 
-// Not all lists need to have keys.A list needs keys if either of the following are true:
+Not all lists need to have keys.A list needs keys if either of the following are true:
 
 // The list - items have memory from one render to the next.For instance, when a to -do list renders, 
 // each item must "remember" whether it was checked off.The items shouldn't get amnesia when they 
