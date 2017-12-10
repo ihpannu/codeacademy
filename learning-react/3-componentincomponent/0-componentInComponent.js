63393e5c395205e48ff3652f81154a69fb4c533f
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-09 22:50:33 -0800

    componentincomponent

---
+++ learning-react/3-componentincomponent/0-componentInComponent.js
@@ -0,0 +1,23 @@
+// A Component in a Render Function
+// Here is a .render() method that returns an HTML-like JSX element:
+
+class Example extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
+// You've seen render methods return <div></div>s, <p></p>s, and <h1></h1>s, just like in the above example.
+
+// Render methods can also return another kind of JSX: component instances.
+
+class OMG extends React.Component {
+  render() {
+    return <h1>Whooaa!</h1>;
+  }
+}
+
+class Crazy extends React.Component {
+  render() {
+    return <OMG />;
+  }
+}
