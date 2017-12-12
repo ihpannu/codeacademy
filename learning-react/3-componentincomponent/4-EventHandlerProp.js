68c7c85ab4ce7467eccae8d83b826cd1b7f20189
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-12 12:15:31 -0800

    chanegs to file

--- learning-react/3-componentincomponent/4-EventHandlerProp.js
+++ learning-react/3-componentincomponent/4-EventHandlerProp.js
@@ -23,10 +23,33 @@ ReactDOM.render(
 
 import React from 'react';
 
+import React from 'react';
+
 export class Button extends React.Component {
     render() {
         return (
-            <button>
+            <button onClick={this.props.talk}>
+                Click me!
+            </button>
+        );
+    }
+}
+// Receive an Event Handler as a prop Great !You just passed a function from <
+// Talker /> to < Button />. In the code editor, select Button.js.Notice that
+// Button renders a < button > </button>element.If a user clicks on this <
+// button > </button>element, then you want your passed - in talk function to
+// get called.That means that you need to attach talk to the < button >
+// same way that you attach any event handler to a JSX element             : you
+// give that JSX element a special attribute.The attribute 's name should be
+// something like onClick or onHover. The attribute' s value should be the event
+// handler that you want to attach.
+
+import React from 'react';
+
+export class Button extends React.Component {
+    render() {
+        return (
+            <button onClick={this.props.talk}>
                 Click me!
             </button>
         );
