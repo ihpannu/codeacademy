4cdab2955534e26d47fd9f0168cce439cda45269
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-12 12:34:24 -0800

    added new folder for props

--- learning-react/3-componentincomponent/4-EventHandlerProp.js
+++ learning-react/3-componentincomponent/4-EventHandlerProp.js
@@ -39,6 +39,7 @@ export class Button extends React.Component {
 // Button renders a < button > </button>element.If a user clicks on this <
 // button > </button>element, then you want your passed - in talk function to
 // get called.That means that you need to attach talk to the < button >
+// </button>as an event handler.How do you do         that             ? The
 // same way that you attach any event handler to a JSX element             : you
 // give that JSX element a special attribute.The attribute 's name should be
 // something like onClick or onHover. The attribute' s value should be the event
