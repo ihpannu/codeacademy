7408b092a5e57c6a168a420d7fa79d8accb674ea
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-18 16:53:09 -0800

    list of sytles

---
+++ React-inline-styles/4-styleValueSyntax.js
@@ -0,0 +1,14 @@
+// In React,
+// if you write a style value as a number,
+// then the unit "px" is assumed.How convenient !If you want a font size of 30 px,
+// you can write : {
+//   fontSize: 30
+// }
+// If you want to use units other than "px," you can use a string : {
+//   fontSize: "2em"
+// }
+// Specifying "px" with a string will still work,
+// although it 's redundant.
+
+// Here is a list of styles that don 't assume "px".
+" https : //reactjs.org/docs/dom-elements.html"
