b0d4562722193fb5bd2fb9e556eeda1fd12d8331
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-18 17:02:08 -0800

    share styles

---
+++ React-inline-styles/5-shareStylesAcross.js
@@ -0,0 +1,5 @@
+// Share Styles Across Multiple Components
+// What if you want to reuse styles for several different components
+//   ? One way to make styles reusable is to keep them in a separate JavaScript file.This file should export the styles that you want to reuse,
+// via export.You can then import your styles into any component that wants them.In the code editor,
+// move back and forth between facebookStyles.js and FacebookColorThief.js to see a styles file in action.
