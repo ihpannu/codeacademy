3a47f9a91c025047681c8ba5c0c78964bc863638
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-21 11:54:41 -0800

    jquery api request

---
+++ react-api-requests/jQueryAPIrequest.js
@@ -0,0 +1,12 @@
+$.ajax({
+  url: 'https://api-to-call.com/endpoint',
+  type: 'GET',
+  dataType: 'json',
+
+  success(response) {
+    console.log(response);
+  },
+  error(jqXHR, status, errorThrown) {
+    console.log(jqZHR);
+  }
+});
