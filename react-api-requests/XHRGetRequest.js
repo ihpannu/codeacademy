fda4195790e82e85e70ed58487bf7b9bc1834f4f
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-21 12:21:50 -0800

    XHR USING JQUERY

--- react-api-requests/XHRGetRequest.js
+++ react-api-requests/XHRGetRequest.js
@@ -3,6 +3,8 @@
 
 // BoilerPlate
 
+
+// To shorten the url 
 // Include data for accessing Google APIs
 
 const apiKey = 'AIzaSyDt68Yxf4uStRD9jQHujqjb4nBKW_P5ehI';
@@ -47,6 +49,75 @@ function shortenUrl() {
 
   xhr.open('POST', urlWithKey);
   xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(data);
+}
+
+function expand() {
+  $responseField.empty();
+  expandUrl();
+  return false;
+}
+
+function shorten() {
+  $responseField.empty();
+  shortenUrl();
+  return false;
+}
+
+// Call functions on submit
+
+$expandButton.click(expand);
+$shortenButton.click(shorten);
+
+
+// TO expand the url using jQuery 
+
+// Include data for accessing Google APIs
+
+const apiKey = 'AIzaSyDt68Yxf4uStRD9jQHujqjb4nBKW_P5ehI';
+const url = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// Some page elements
+
+const $inputField = $('#input');
+const $expandButton = $('#expand');
+const $shortenButton = $('#shorten');
+const $responseField = $('#responseField');
+
+// AJAX functions
+
+function expandUrl() {
+  const urlToExpand = url + '?key=' + apiKey + '&shortUrl=' + $inputField.val();
+  $.ajax({
+    url: urlToExpand,
+    type: 'GET',
+    dataType: 'json',
+
+    success(response) {
+      $responseField.append('<p>Your expanded url is: </p><p>' + response.longUrl + '</p>');
+    },
+
+    error(jqXHR, status, errorThrown) {
+      console.log(jqXHR)
+    }
+  });
+}
+
+function shortenUrl() {
+  const urlWithKey = url + '?key=' + apiKey;
+  const urlToShorten = inputField.val();
+  const data = JSON.stringify({longUrl: urlToShorten});
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      $responseField.append('<p>Your shortened url is: </p><p>' + xhr.response.id + '</p>');
+    }
+  };
+
+  xhr.open('POST', urlWithKey);
+  xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(data);
 }
 
