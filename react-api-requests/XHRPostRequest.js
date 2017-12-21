4da0e96acece7b19d73ea5571b287415760d3aac
Harmandeep Pannu <iharmanpannu@gmail.com>
2017-12-21 12:26:44 -0800

    post request using jquery

--- react-api-requests/XHRPostRequest.js
+++ react-api-requests/XHRPostRequest.js
@@ -13,3 +13,19 @@ xhr.onreadystatechange = function () {
 
 xhr.open('POST', url);
 xhr.send(data);
+
+// USING JQUERY 
+
+$.ajax({
+  url: 'https://api-to-call.com/endpoint',
+  type: 'POST',
+  data: JSON.stringify({id: 200}),
+  dataType: 'json',
+  success(response) {
+    console.log(response);
+  },
+  
+  error(jqXHR, status, errorThrown) {
+    console.log(jqXHR);
+  }
+}); 
