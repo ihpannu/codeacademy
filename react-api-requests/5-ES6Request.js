9d337593886beb376cdb46336cb5abb1684647ff
iharmanpannu <iharmanpannu@gmail.com>
2017-12-22 11:00:06 -0800

    added funciton with arrow function

--- react-api-requests/5-ES6Request.js
+++ react-api-requests/5-ES6Request.js
@@ -65,3 +65,14 @@ function shorten() {
 
 $expandButton.click(expand);
 $shortenButton.click(shorten);
+
+
+fetch('https://api-to-call.com/endpoint')
+.then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => console.log(networkError.message)
+
+).then(jsonResponse => jsonResponse );
